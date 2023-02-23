5cf8d59bb9cdb40d1952c97753c627b4f39d98f2
grepp <grepp@grepp.co>
2023-02-23 13:48:32 +0900

    add maven files

---
+++ src/main/java/project/App.java
@@ -0,0 +1,15 @@
+package project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello world");
+        
+        // 아래에 문제 해결을 위한 코드를 작성해주세요.
+    }
+}
